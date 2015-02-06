0bb2271b5c226bd499befcb7d484ff45ab8018b8
Franck DENIS <franck@mailfr.com>
2015-02-06 10:01:30 +0100

    Active Sheet and getSheetRow
    getSheetRow can be used for large files

--- xlsxreader.class.php
+++ xlsxreader.class.php
@@ -10,11 +10,27 @@ class XLSXReader
 	protected $decodeUTF8 = false;
 
 	protected $sheets        = array();
+	protected $activeSheet   = 0;
 	protected $sharedStrings = array();
+
+	protected $currentXML    = null;
 
 	public function __construct($xlsxFile)
 	{
-		$this->xlsxFile = $xlsxFile;
+		if (file_exists($xlsxFile) === false)
+		{
+			Throw New Exception("XLSXReader: XLSX file not found");
+		}
+
+		$this->xlsxFile = realpath($xlsxFile);
+	}
+
+	public function __destruct()
+	{
+		if (is_null($this->currentXML) === false)
+		{
+			$this->currentXML->close();
+		}
 	}
 
 	public function read()
@@ -33,6 +49,16 @@ class XLSXReader
 		$this->decodeUTF8 = (is_bool($value) ? $value : true);
 	}
 
+	public function getActiveSheet()
+	{
+		$sheetKeys = array_keys($this->sheets);
+
+		if (is_null($this->activeSheet))
+			return $sheetKeys[0];
+
+		return $sheetKeys[$this->activeSheet];
+	}
+
 	protected function readWorkbook()
 	{
 		$xml = new XMLReader();
@@ -45,6 +71,11 @@ class XLSXReader
 
 		while ($xml->read())
 		{
+			if ($xml->name === "workbookView" && $xml->nodeType == XMLReader::ELEMENT)
+			{
+				$this->activeSheet = $xml->getAttribute("activeTab");
+			}
+
 			if ($xml->name === "sheet" && $xml->nodeType == XMLReader::ELEMENT)
 			{
 				$rID = $xml->getAttribute("r:id");
@@ -119,6 +150,104 @@ class XLSXReader
 	public function getSheets()
 	{
 		return $this->sheets;
+	}
+
+	public function getSheetRow($sheetID)
+	{
+		if (isset($this->sheets[$sheetID]) === false)
+			return false;
+
+		$sheet = $this->sheets[$sheetID];
+
+		if (is_null($this->currentXML))
+		{
+			$this->currentXML = new XMLReader();
+
+			$xml = &$this->currentXML;
+
+			if (($result = $xml->open("zip://" . $this->xlsxFile . "#xl/" . $sheet["file"])) === false)
+			{
+				$xml->close();
+				return false;
+			}
+
+			# Move to first row
+			while ($xml->read() && $xml->name !== "row");
+		}
+		else
+		{
+			$xml = &$this->currentXML;
+			
+			if ($xml->next("row") === false)
+			{
+				$xml->close();
+				$this->currentXML = null;
+
+				return array();
+			}
+		}
+
+		$cols = array();
+
+		if ($xml->name === "row")
+		{
+			/**
+			* attribut 't'
+			* - b for boolean
+			* - d for date
+			* - inlineStr for an inline string
+			* - n for number
+			* - s for shared string
+			* - str for a formula
+			*/
+			$node = $xml->expand();
+
+			if ($node->hasChildNodes() === true)
+			{
+				foreach ($node->childNodes as $cellNode)
+				{
+					# For each cell 'c'
+					if ($cellNode->nodeName === "c")
+					{
+						$cellType  = $cellNode->getAttribute("t");
+						$vTags     = $cellNode->getElementsByTagName("v");
+
+						$cellNum   = $cellNode->getAttribute("r");
+						$cellNum   = preg_replace("/[0-9]+$/", "", $cellNum);
+						$cellNum   = $this->excelLettersToNum($cellNum);
+						$cellNum   = $cellNum - 1;
+
+						$cellValue = "";
+
+						foreach ($vTags as $vTag)
+						{
+							$cellValue = $vTag->nodeValue;
+						}
+
+						if (empty($cellType))
+						{
+							if (($num = filter_var($cellValue, FILTER_VALIDATE_FLOAT)) !== false)
+							{
+								$cellValue = $num;
+							}
+						}
+						elseif ($cellType === "s") # sharedStrings
+						{
+							$cellValue = (isset($this->sharedStrings[$cellValue]) ? $this->sharedStrings[$cellValue] : "#");
+						}
+
+						$cols[$cellNum] = $this->decodeString($cellValue);
+					}
+				}
+			}
+		}
+		else
+		{
+			$xml->close();
+			$this->currentXML = null;
+		}
+
+		return $cols;
 	}
 
 	public function getSheetDatas($sheetID)
